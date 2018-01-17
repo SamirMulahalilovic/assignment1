ffa98ccbce5045f98222090cf2ba4c5e4d65ca02
u0897227 <u0897227@student-checkout-mac-laptop-57.uconnect.utah.edu>
2018-01-17 14:29:02 -0700

    Added comments

--- HW1/main.swift
+++ HW1/main.swift
@@ -1,21 +1,26 @@
 //
 //  main.swift
 //  HW1
-//
+//  User inputs (Item, Item: Quantity, or Blank if finished)
 //  Created by u0897227 on 1/16/18.
 //  Copyright © 2018 u0897227. All rights reserved.
 //
 
 import Foundation
 
+// creates boolean flag
 var flag = false
+// creates dictionary
 var dictItems = [String: Int]()
 print("Enter Shopping List Item: (Item, Item: Quantity, or blank if finished)")
 while(!flag){
+    // grab input
     if let response = readLine(){
         if(!response.isEmpty){
+            // seperate name and quantity
             var twoParts = response.components(separatedBy: ": ")
             var quantity = 0
+            // if there is no quantity, assume it is suppose to be 1
             if (twoParts.count == 1)
             {
                 quantity = 1
@@ -23,6 +28,7 @@ while(!flag){
             else{
                 quantity = Int(twoParts[1])!
             }
+            // lowercase the words to match Apple with apple
             twoParts[0] = twoParts[0].lowercased()
             if let item = dictItems[twoParts[0]] {
                 dictItems[twoParts[0]] = item + quantity
@@ -32,8 +38,10 @@ while(!flag){
             }
         }
         else{
+            // end the loop
             flag = true
             print("finalizing list")
+            // print out the keys with their quantities in sorted order
             for (k,v) in Array(dictItems).sorted(by: <){
                 print("\(k): \(v)")
             }
