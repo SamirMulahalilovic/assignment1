562c8f17ab65920cbc42e3e562c86575569ca3a8
u0897227 <u0897227@student-checkout-mac-laptop-57.uconnect.utah.edu>
2018-01-17 14:20:28 -0700

    Final touches

--- HW1/main.swift
+++ HW1/main.swift
@@ -8,13 +8,37 @@
 
 import Foundation
 
-
+var flag = false
 var dictItems = [String: Int]()
-print("Enter Shopping List Item: (Item, Item + Quantity, or blank if finished)")
-if let response = readLine(){
-    print("added \(response)!")
-} else{
-    print("Generating Shopping List")
+print("Enter Shopping List Item: (Item, Item: Quantity, or blank if finished)")
+while(!flag){
+    if let response = readLine(){
+        if(!response.isEmpty){
+            var twoParts = response.components(separatedBy: ": ")
+            var quantity = 0
+            if (twoParts.count == 1)
+            {
+                quantity = 1
+            }
+            else{
+                quantity = Int(twoParts[1])!
+            }
+            twoParts[0] = twoParts[0].lowercased()
+            if let item = dictItems[twoParts[0]] {
+                dictItems[twoParts[0]] = item + quantity
+            }
+            else {
+                dictItems[twoParts[0]] = quantity
+            }
+        }
+        else{
+            flag = true
+            print("finalizing list")
+            for (k,v) in Array(dictItems).sorted(by: <){
+                print("\(k): \(v)")
+            }
+        }
+    }
 }
 
 
